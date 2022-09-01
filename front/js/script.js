b82d0429274589031a18d235dd0160ef43e4683a
dphiane <108275807+dphiane@users.noreply.github.com>
2022-09-01 13:35:33 +0200

    suppression innerHtml

--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,44 @@
-
-    const URL = "http://localhost:3000/api/products";
-    
-  let cardsFetch= function(){
-    fetch(URL)
-	.then(response => response.json())
-	.then((data) => {console.log(data)
-
     let items= document.getElementById("items");//récuperez items dans HTML
 
-        for (i=0; i < data.length; i++){//Appel des produits dans le tableau
-            const PRODUCTCARD=`
-                <a href="./product.html?id=${data[i]._id}">
-                <article>
-                <img src="${data[i].imageUrl}" alt="${data[i].altTxt}">
-                <h3 class="productName">${data[i].name}</h3>
-                <p class="productDescription">${data[i].description}</p>
-                </article>
-                </a> -->`;
-            items.innerHTML +=PRODUCTCARD;//Ajout des constant HTML
-        }})
-    };
-    cardsFetch();//Appel fonction
+    function getDataApi(){
+        fetch ("http://localhost:3000/api/products")
+        .then(response=>response.json())
+        .then(function(produits){
+            for (let produit of produits){
+                insertProduitsHtml(produit)
+            }
+        })
+    }
+
+    getDataApi()
+
+    function insertProduitsHtml(produit){
+        let ancre=document.createElement("a")
+        ancre.href="./produit.html?id=" + produit.__id
+
+        let article=document.createElement('article')
+        let image=document.createElement('img')
+        image.src=produit.imageUrl
+        image.alt=produit.altTxt
+
+        let title=document.createElement("h3")
+        title.classList.add("productName")
+        title.innerText=produit.name
+
+        let paragraph= document.createElement('p')
+        paragraph.classList.add("productDescription")
+        paragraph.innerText=produit.description
+
+        let price=document.createElement("p")
+        price.classList.add("price")
+        price.innerText="Prix = "+ produit.price + "€"
+
+        article.appendChild(image)
+        article.appendChild(title)
+        article.appendChild(paragraph)
+        article.appendChild(price)
+        ancre.appendChild(article)
+        
+        items.appendChild(ancre)
+
+    }
